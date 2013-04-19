4fe6b617b63f14abd5a6c0839b6b4046c6fc5b6a
Lucas Polo <pradopolo@gmail.com>
2013-04-19 12:47:39 -0300

    JAX JMS EJB

---
+++ workspace-fj31/fj31-jaxb-standalone/src/br/com/caelum/fj31/jaxb/Categoria.java
@@ -0,0 +1,16 @@
+package br.com.caelum.fj31.jaxb;
+
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name="CAT")
+public class Categoria {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
